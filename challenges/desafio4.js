d0659176997fbf2bdc9d04fcab0a4ce8a43c7860
jefersongjr <jeferson_gjr@hotmail.com>
2022-11-09 16:50:06 -0300

    feat: requisito 4 - filtrando mais vendidos acima de 50 e abaixo de 100

---
+++ challenges/desafio4.js
@@ -0,0 +1,10 @@
+db.produtos.find({
+    $and: [
+        { vendidos: { $lt: 100 } },
+        { vendidos: { $gt: 50 } },
+    ] },
+    {
+        _id: 0,
+        nome: 1, 
+        vendidos: 1,
+    }).sort({ vendidos: 1 });
